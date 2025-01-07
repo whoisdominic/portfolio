9cd500461a9619b7a257286fcabd3f0dcf959523
Dominic <dominicshajaa@icloud.com>
2025-01-07 14:05:04 -0800

    stats fixed

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -25,13 +25,13 @@ const stats = [
 
 const Stats = () => {
   return (
-    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
+    <section className="pt-4 pb-16 xl:pt-0 xl:pb-0">
       <div className="container mx-auto">
-        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+        <div className="flex flex-col flex-wrap justify-center lg:flex-row  gap-6 xl:flex-wrap ">
           {stats.map((item, index) => {
             return (
               <div
-                className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
+                className="flex-1 flex gap-4 items-center justify-between px-16 lg:justify-start lg:px-0"
                 key={index}
               >
                 <CountUp
@@ -42,11 +42,7 @@ const Stats = () => {
                   prefix={item.prefix}
                   suffix={item.suffix}
                 />
-                <p
-                  className={`${
-                    item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
-                  } leading-snug text-white/80`}
-                >
+                <p className={"max-w-[80px] leading-snug text-white/80"}>
                   {item.text}
                 </p>
               </div>
